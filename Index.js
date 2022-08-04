9cd9486bf022447948cb4b399bb5c5453c1ad24b
MorganGray28 <robertmgray2828@gmail.com>
2022-08-03 20:29:02 -0700

    Add Airtable database connection to server

--- Index.js
+++ Index.js
@@ -8,8 +8,16 @@ const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
-
-//TODO: Set up Airtable API and connect to our demo database
+const Airtable = require('airtable');
+Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: process.env.AIRTABLE_API_KEY
+});
+const base = Airtable.base(prcess.env.AIRTABLE_BASE);
+app.use(cookieParser(process.env.AUTH_RANDOM_KEY));
+app.use(basicAuth({
+  users: { 'guest': 'password' }
+}));
 
 
 // Middleware:
@@ -19,7 +27,8 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-
+// TODO: Finish setting up routes 
+// Routes:
 app.get('/', (req, res) => {
   res.send('hello world');
 });
