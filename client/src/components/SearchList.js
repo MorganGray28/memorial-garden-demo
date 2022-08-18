b0a2335ca7bb625be476398f18ecbcf86ef481fc
MorganGray28 <robertmgray2828@gmail.com>
2022-08-17 20:28:25 -0700

    Adjust styling of search list

--- client/src/components/SearchList.js
+++ client/src/components/SearchList.js
@@ -2,23 +2,37 @@ import { Box } from '@mui/system';
 import React from 'react';
 
 export default function SearchList(props) {
-    function handleClick(e) {
-        props.setActive(props.id);
-    }
+	function handleClick(e) {
+		props.setActive(props.id);
+	}
 
-    let backgroundStyle;
-    if (props.currentActive) {
-        if (props.currentActive.id === props.location) {
-            backgroundStyle = '#E3F0FF';
-        } else {
-            backgroundStyle = '';
-        }
-    }
+	let backgroundStyle;
+	if (props.currentActive) {
+		if (props.currentActive.id === props.location) {
+			backgroundStyle = '#E3F0FF';
+		} else {
+			backgroundStyle = '';
+		}
+	}
 
-    return (
-        <Box p={1} sx={{display: 'flex', backgroundColor: backgroundStyle, justifyContent:'space-between', borderBottom:'1px solid rgb(140,140,140)', '&:hover': {cursor:'pointer', backgroundColor:'#F2F7FD'}}} onClick={handleClick}>
-            <span style={{paddingLeft:'15px'}} className="SearchList-category">{props.headstone ? props.headstone : 'N/A'}</span>
-            <span style={{paddingRight:'15px'}} className="SearchList-category">{props.location}</span>
-        </Box>
-    )
+	return (
+		<Box
+			p={1}
+			sx={{
+				display: 'flex',
+				backgroundColor: backgroundStyle,
+				justifyContent: 'space-between',
+				borderBottom: '1px solid rgb(224 224 224)',
+				'&:hover': { cursor: 'pointer', backgroundColor: '#F2F7FD' },
+			}}
+			onClick={handleClick}
+		>
+			<span style={{ paddingLeft: '15px' }} className='SearchList-category'>
+				{props.headstone ? props.headstone : 'N/A'}
+			</span>
+			<span style={{ paddingRight: '15px' }} className='SearchList-category'>
+				{props.location}
+			</span>
+		</Box>
+	);
 }
