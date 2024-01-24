e6e8622e28df666d61d4563a8bff12593b0cb64d
MorganGray28 <robertmgray2828@gmail.com>
2024-01-24 15:39:20 -0800

    Replace airtable apiKey with personal access token

--- Index.js
+++ Index.js
@@ -10,15 +10,14 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 const Airtable = require('airtable');
 Airtable.configure({
-    endpointUrl: 'https://api.airtable.com',
-    apiKey: process.env.AIRTABLE_API_KEY
+	endpointUrl: 'https://api.airtable.com',
+	apiKey: process.env.AIRTABLE_PERSONAL_ACCESS_TOKEN,
 });
 const base = Airtable.base(process.env.AIRTABLE_BASE);
 app.use(cookieParser(process.env.AUTH_RANDOM_KEY));
-const auth = (basicAuth({
-  users: { 'admin': 'password' }
-}));
-
+const auth = basicAuth({
+	users: { admin: 'password' },
+});
 
 // Middleware:
 app.use(helmet());
@@ -27,103 +26,110 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-// TODO: Finish setting up routes 
+// TODO: Finish setting up routes
 // Routes:
-app.get('/', (req,res) =>{
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 app.get('/plots', (req, res) => {
-  if(req.signedCookies.name !== 'admin') {
-      res.end();
-  }
-  let results = retrieveAllPlots();
-  results
-      .then(data => {
-          res.send(data);
-      })
-      .catch(err => {
-          console.log(err);
-      });
+	if (req.signedCookies.name !== 'admin') {
+		res.end();
+	}
+	let results = retrieveAllPlots();
+	results
+		.then((data) => {
+			res.send(data);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 });
 
 app.post('/plots/edit/:airtableId', (req, res) => {
-  const plotAirtableId = req.params.airtableId;
-  const updates = req.body;
-  let result = retrievePlot(plotAirtableId, updates)
-      .then(data => {
-          res.send(data);
-      })
-      .catch(err => {
-          res.send(err);
-      });
+	const plotAirtableId = req.params.airtableId;
+	const updates = req.body;
+	let result = retrievePlot(plotAirtableId, updates)
+		.then((data) => {
+			res.send(data);
+		})
+		.catch((err) => {
+			res.send(err);
+		});
 });
 
 app.get('/authenticate', auth, (req, res) => {
-  const options = {
-      httpOnly: true,
-      signed: true,
-      maxAge: 1000 * 60 * 60 * 24 * 2
-  }
-  if (req.auth.user === 'admin') {
-      res.cookie('name', 'admin', options).send({ screen: 'admin' });
-  }
+	const options = {
+		httpOnly: true,
+		signed: true,
+		maxAge: 1000 * 60 * 60 * 24 * 2,
+	};
+	if (req.auth.user === 'admin') {
+		res.cookie('name', 'admin', options).send({ screen: 'admin' });
+	}
 });
 
 app.get('/read-cookie', (req, res) => {
-  if (req.signedCookies.name === 'admin') {
-    res.send({ screen: 'admin' });
-  } else {
-    res.send({ screen: 'auth' });
-  }
+	if (req.signedCookies.name === 'admin') {
+		res.send({ screen: 'admin' });
+	} else {
+		res.send({ screen: 'auth' });
+	}
 });
 
 app.get('/clear-cookie', (req, res) => {
-  res.clearCookie('name').end();
+	res.clearCookie('name').end();
 });
 
 // Get Plot Data
 function retrieveAllPlots() {
-  let allRecords = [];
-  const getPlots = new Promise((resolve, reject) => {
-      base('Table 1').select({
-          // Selecting the first 3 records in Grid view:
-          view: "Grid view",
-          sort: [{field: 'headstone', direction: 'asc'}]
-      }).eachPage(function page(records, fetchNextPage) {
-          // This function (`page`) will get called for each page of records.
-      
-          records.forEach(function(record) {
-              // console.log('Retrieved', record.get('location'));
-              allRecords.push(record);
-          });
-      
-          // To fetch the next page of records, call `fetchNextPage`.
-          // If there are more records, `page` will get called again.
-          // If there are no more records, `done` will get called.
-          fetchNextPage();
-      
-      }, function done(err) {
-          resolve(allRecords);
-          if (err) { reject(err);}
-      });
-  });
-  return getPlots;
+	let allRecords = [];
+	const getPlots = new Promise((resolve, reject) => {
+		base('Table 1')
+			.select({
+				// Selecting the first 3 records in Grid view:
+				view: 'Grid view',
+				sort: [{ field: 'headstone', direction: 'asc' }],
+			})
+			.eachPage(
+				function page(records, fetchNextPage) {
+					// This function (`page`) will get called for each page of records.
+
+					records.forEach(function (record) {
+						// console.log('Retrieved', record.get('location'));
+						allRecords.push(record);
+					});
+
+					// To fetch the next page of records, call `fetchNextPage`.
+					// If there are more records, `page` will get called again.
+					// If there are no more records, `done` will get called.
+					fetchNextPage();
+				},
+				function done(err) {
+					resolve(allRecords);
+					if (err) {
+						reject(err);
+					}
+				}
+			);
+	});
+	return getPlots;
 }
 
 function retrievePlot(id, updates) {
-  const plot = new Promise ((resolve, reject) => {
-      base('Table 1').update(id, updates, function(err, record) {
-          if (err) {
-            console.error(err); reject(err);
-          }
-          resolve(record)
-        });
-      });
+	const plot = new Promise((resolve, reject) => {
+		base('Table 1').update(id, updates, function (err, record) {
+			if (err) {
+				console.error(err);
+				reject(err);
+			}
+			resolve(record);
+		});
+	});
 
-  return plot;
+	return plot;
 }
 
 app.listen(PORT, () => {
-  console.log(`Example server running on port ${PORT}`);
+	console.log(`Example server running on port ${PORT}`);
 });
